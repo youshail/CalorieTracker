44f51f075f241193f8f94769b844de693273c339
YOUSSEF SOUHAIL <s.youssef91@gmail.com>
2023-08-02 11:49:41 +0100

    Setting Local Data Base

---
+++ tracker/tracker_data/src/main/java/com/youshail/tracker_data/local/TrackerDao.kt
@@ -0,0 +1,29 @@
+package com.youshail.tracker_data.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.youshail.tracker_data.local.entity.TrackedFoodEntity
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface TrackerDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTrackerFood(trackedFoodEntity: TrackedFoodEntity)
+
+    @Delete
+    suspend fun deleteTrackerFood(trackedFoodEntity: TrackedFoodEntity)
+
+    @Query(
+        """
+            SELECT *
+            FROM trackedfoodentity
+            WHERE dayOfMonth = :day AND month = :month AND year= :year
+        """
+    )
+    fun getFoodsForDate(day: Int, month: Int, year: Int): Flow<List<TrackedFoodEntity>>
+}

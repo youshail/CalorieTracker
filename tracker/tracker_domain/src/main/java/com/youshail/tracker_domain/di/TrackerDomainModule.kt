a2aa9276e9225014411bee163cdb31db8c9b4877
YOUSSEF SOUHAIL <s.youssef91@gmail.com>
2023-08-07 17:30:47 +0100

    Day Selector

--- tracker/tracker_domain/src/main/java/com/youshail/tracker_domain/di/TrackerDomainModule.kt
+++ tracker/tracker_domain/src/main/java/com/youshail/tracker_domain/di/TrackerDomainModule.kt
@@ -18,7 +18,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(ViewModelComponent::class)
-class TrackerDomainModule {
+object TrackerDomainModule {
 
     @ViewModelScoped
     @Provides
--- tracker/tracker_presentation/src/main/java/com/youshail/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/youshail/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,6 +1,9 @@
 package com.youshail.tracker_presentation.tracker_overview
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
@@ -9,6 +12,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.youshail.core.util.UiEvent
 import com.youshail.core_ui.LocalSpacing
+import com.youshail.tracker_presentation.tracker_overview.components.DaySelector
 import com.youshail.tracker_presentation.tracker_overview.components.NutrientsHeader
 
 
@@ -26,6 +30,20 @@ fun TrackerOverviewScreen(
         .padding(bottom = spacing.spaceSmall)){
         item { 
             NutrientsHeader(state = state)
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = state.date,
+                onPreviousDayClick = {
+                                     viewModel.onEvent(TrackerOverviewEvent.OnPreviousDayClick)
+                },
+                onNextDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnNextDayClick)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
         }
     }
 }

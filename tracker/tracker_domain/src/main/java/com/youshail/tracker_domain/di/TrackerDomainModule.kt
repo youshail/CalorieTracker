38bf974e011e75fbf5b5c5651e0d78f765b48944
YOUSSEF SOUHAIL <s.youssef91@gmail.com>
2023-08-10 09:22:58 +0100

    Expandable Meal UI

--- tracker/tracker_domain/src/main/java/com/youshail/tracker_domain/di/TrackerDomainModule.kt
+++ tracker/tracker_domain/src/main/java/com/youshail/tracker_domain/di/TrackerDomainModule.kt
@@ -13,7 +13,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
-import javax.inject.Singleton
+
 
 
 @Module
--- tracker/tracker_presentation/src/main/java/com/youshail/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/youshail/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,21 +1,30 @@
 package com.youshail.tracker_presentation.tracker_overview
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.annotation.ExperimentalCoilApi
+import com.youshail.core.R
 import com.youshail.core.util.UiEvent
 import com.youshail.core_ui.LocalSpacing
+import com.youshail.tracker_presentation.tracker_overview.components.AddButton
 import com.youshail.tracker_presentation.tracker_overview.components.DaySelector
+import com.youshail.tracker_presentation.tracker_overview.components.ExpandableMeal
 import com.youshail.tracker_presentation.tracker_overview.components.NutrientsHeader
+import com.youshail.tracker_presentation.tracker_overview.components.TrackedFoodItem
 
 
+@ExperimentalCoilApi
 @Composable
 fun TrackerOverviewScreen(
     onNavigate: (UiEvent.Navigate) -> Unit,
@@ -44,6 +53,48 @@ fun TrackerOverviewScreen(
                     .padding(horizontal = spacing.spaceMedium)
             )
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+
+        items(state.meals) { meal ->
+            ExpandableMeal(
+                meal = meal,
+                onToggleClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClick(meal))
+                },
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = spacing.spaceSmall)
+                    ) {
+                        state.trackedFoods.forEach { food ->
+                            TrackedFoodItem(
+                                trackedFood = food,
+                                onDeleteClick = {
+                                    viewModel.onEvent(
+                                        TrackerOverviewEvent
+                                            .OnDeleteTrackedFoodClick(food)
+                                    )
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+                        }
+                        AddButton(
+                            text = stringResource(
+                                id = R.string.add_meal,
+                                meal.name.asString(context)
+                            ),
+                            onClick = {
+                                viewModel.onEvent(
+                                    TrackerOverviewEvent.OnAddFoodClick(meal)
+                                )
+                            },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
